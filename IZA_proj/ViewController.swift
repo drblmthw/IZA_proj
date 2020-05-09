7f9ae00d8c93c34b57f70fd8b319485bb0ead942
drblmthw <matej.kubovy@icloud.com>
2020-05-09 12:39:57 +0200

    Initial Commit

---
+++ IZA_proj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IZA_proj
+//
+//  Created by Matej Kubový on 09/05/2020.
+//  Copyright © 2020 Matej Kubový. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
